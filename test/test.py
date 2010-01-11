a57c817031c4771b78f1360533a9cbde7aed2bd0
atofigh <alix.tofigh@gmail.com>
2010-01-11 14:21:38 +0000

    Increased size and number of tests. Testing should be improved to allow testing of specific cases such as the one in bug ID 2929814 Output contains cycle.

--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@ import os
 print " Test: small complete graphs ".center(80, '=')
 print
 
-test_params = ["0 1 4 80", "0 2 4 80", "-1 4 4 80", "-1 4 6 80", "-10 30 6 80", "-30 10 6 80"]
+test_params = ["0 1 4 80", "0 2 4 80", "-1 4 4 80", "-1 4 6 80", "-10 30 6 80", "-30 10 6 80", "-10 30 7 80", "-10 30 7 80", "-10 30 7 80"]
 
 for params in test_params:
     if os.system("./test " + params) != 0:
